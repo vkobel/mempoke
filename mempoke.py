5972b02acfb69ff2d6eaa86cf5c83a9b17f636ae
Vincent Kobel <v@kobl.one>
2019-09-02 18:20:25 +0200

    refactor in classes

--- mempoke.py
+++ mempoke.py
@@ -1,110 +1,114 @@
 #!/usr/bin/env python3
 
-from pprint import pprint
+from collections import namedtuple
+from typing import NamedTuple
+from pprint import pformat, pprint
 import argparse
 import sys
 
+SeekResult = namedtuple('SeekResult', ['data', 'address', 'region'])
 
-def get_memory_info(pids, filter=None):
-    ret = []
-    for pid in pids:
+
+class MemoryRegion(NamedTuple):
+    name: str
+    start: int
+    end: int
+    perm: str
+    size: int
+    pid: int
+
+    def read(self):
+        with open(f"/proc/{self.pid}/mem", "r+b") as mem_file:
+            mem_file.seek(self.start)
+            try:
+                return mem_file.read(self.size)
+            except OSError:
+                return bytes()
+
+    def read_at_address(self, rel_address, nb_bytes=None):
+        mem_data = self.read()
+
+        if nb_bytes and type(nb_bytes) == int:
+            # if read-bytes flag is used, read that amount of bytes
+            return mem_data[rel_address:rel_address + nb_bytes]
+        else:
+            # read until next zero byte or at max 150
+            until_zero = mem_data.find(b'\x00', rel_address)
+            if until_zero - rel_address > 150:
+                until_zero = rel_address + 150
+            return mem_data[rel_address:until_zero]
+
+
+class ProcessMemory:
+    def __init__(self, pid, region_filter=None):
+        self.pid = pid
+        self.regions = []
+
+        total_size = 0
         try:
             with open(f"/proc/{pid}/maps", "r") as maps_file:
                 for line in maps_file:
                     region_info = line.split()
-                    addrs = region_info[0].split("-")
-                    perm = region_info[1]
-                    region = region_info[5] if len(region_info) > 5 else addrs[0]
+                    addrs = list(map(lambda x: int(x, 16), region_info[0].split("-")))
 
-                    info = {"region": region,
-                            "start": addrs[0],
-                            "end": addrs[1],
-                            "perm": perm,
-                            "size": int(addrs[1], 16) - int(addrs[0], 16),
-                            "pid": pid}
+                    region = MemoryRegion(
+                        name=region_info[5] if len(region_info) > 5 else addrs[0],
+                        start=addrs[0],
+                        end=addrs[1],
+                        perm=region_info[1],
+                        size=addrs[1] - addrs[0],
+                        pid=self.pid
+                    )
 
                     # cumulative search of all filter items in a simple serialization of "info" dict
-                    lookup_str = ''.join(map(str, info.values()))
-                    if not filter or all(lookup_str.find(f_itm) >= 0 for f_itm in filter):
-                        ret.append(info)
+                    lookup_str = ''.join(map(str, region._asdict().values()))
+                    if not region_filter or all(lookup_str.find(f_itm) >= 0 for f_itm in region_filter):
+                        self.regions.append(region)
+                        total_size += region.size
+
+                self.total_size_kib = total_size // 1024
 
         except FileNotFoundError:
-            print("PID not found!")
-            quit(1)
+            raise Exception(f"PID '{self.pid}' not found!")
 
-    return ret
+    def __write(self, address, data):
+        with open(f"/proc/{self.pid}/mem", "r+b") as mem_file:
+            mem_file.seek(address)
+            mem_file.write(bytes(data.replace("\\x00", "\x00"), "ASCII"))
+            mem_file.flush()
 
+    def seek_memory(self, seek, write=None, read=None):
+        for region in self.regions:
+            last_addr = 0
 
-def get_memory_data(mem_info):
-    ret = []
-    for region in mem_info:
-        with open(f"/proc/{region['pid']}/mem", "r+b") as mem_file:
-            mem_file.seek(int(region["start"], 16))
-            try:
-                mem = mem_file.read(region["size"])
-                ret.append(mem)
-            except OSError:
-                ret.append(bytes())
-    return ret
+            # if seeking an address (starting with '0x'), just go directly there without looping
+            if seek[:2] == '0x':
+                dec_addr = int(seek, 16)
+                if region.start <= dec_addr and region.end > dec_addr:
 
+                    if write:
+                        self.__write(dec_addr, write)
 
-def read_memory(mem_data, rel_address, nb_bytes=None):
-    if nb_bytes and type(nb_bytes) == int:
-        # if read-bytes flag is used, read that amount of bytes
-        return mem_data[rel_address:rel_address + nb_bytes]
-
-    else:
-        # read until next zero byte or at max 150
-        until_zero = mem_data.find(b'\x00', rel_address)
-        if until_zero - rel_address > 150:
-            until_zero = rel_address + 150
-        return mem_data[rel_address:until_zero]
-
-
-def write_memory(pid, address, data):
-    with open(f"/proc/{pid}/mem", "r+b") as mem_file:
-        mem_file.seek(address)
-        mem_file.write(bytes(data.replace("\\x00", "\x00"), "ASCII"))
-        mem_file.flush()
-
-
-def seek_memory(mem_info, mem_data, seek=None, write=None, read=None):
-    results = []
-
-    for inf, mem in zip(mem_info, mem_data):
-        last_addr = 0
-
-        # if seeking an address (starting with '0x'), just go directly there without looping
-        if seek[:2] == '0x':
-            dec_addr = int(seek, 16)
-            start = int(inf['start'], 16)
-            end = int(inf['end'], 16)
-            if start <= dec_addr and end > dec_addr:
-
-                if write:
-                    write_memory(inf["pid"], dec_addr, write)
-                    results.append({'at': seek, 'info': inf})
-                else:
-                    read_data = read_memory(mem, dec_addr - start, nb_bytes=read)
-                    results.append({'read': read_data, 'at': seek, 'info': inf})
-                break
-        else:
-            while True:
-                offset = mem.find(seek.encode(), last_addr)
-                if offset < 0:
+                    read_data = region.read_at_address(dec_addr - region.start, nb_bytes=read)
+                    yield SeekResult(read_data, seek, region.name)
                     break
+            else:
+                while True:
+                    mem = region.read()
+                    offset = mem.find(seek.encode(), last_addr)
+                    if offset < 0:
+                        break
 
-                absolute_addr = int(inf["start"], 16) + offset
-                if write:
-                    write_memory(inf["pid"], absolute_addr, write)
-                    results.append({'at': hex(absolute_addr), 'info': inf})
-                else:
-                    read_data = read_memory(mem, offset, nb_bytes=read)
-                    results.append({'read': read_data, 'at': hex(absolute_addr), 'info': inf})
+                    absolute_addr = region.start + offset
+                    if write:
+                        self.__write(absolute_addr, write)
 
-                last_addr = offset + 1
+                    read_data = region.read_at_address(offset, nb_bytes=read)
+                    last_addr = offset + 1
+                    yield SeekResult(read_data, hex(absolute_addr), region.name)
 
-    return results
+    def __str__(self):
+        return '\n'.join(map(lambda r: pformat(r._asdict()), self.regions))
 
 
 if __name__ == "__main__":
@@ -122,20 +126,18 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.dump:
-        mem_info = get_memory_info(args.pids, args.filter)
-        mem_data = get_memory_data(mem_info)
-        for mem in mem_data:
-            sys.stdout.buffer.write(mem)
+    processes = map(lambda pid: ProcessMemory(pid, args.filter), args.pids)
 
-        if len(mem_info) > 1:
+    if args.dump:
+        all_regions = [region for process in processes for region in process.regions]
+        for region in all_regions:
+            sys.stdout.buffer.write(region.read())
+
+        if len(all_regions) > 1:
             sys.stderr.buffer.write(b"\n\nWarning multiple memory regions have been dump")
 
     elif args.seek:
-        mem_info = get_memory_info(args.pids, args.filter)
-        mem_data = get_memory_data(mem_info)
-
-        results = seek_memory(mem_info, mem_data, args.seek, write=args.write, read=args.read_bytes)
+        results = list(map(lambda p: list(p.seek_memory(args.seek, write=args.write, read=args.read_bytes)), processes))
         if not results:
             print("pattern not found")
             quit(1)
@@ -143,4 +145,4 @@ if __name__ == "__main__":
             pprint(results)
 
     else:
-        pprint(get_memory_info(args.pids, args.filter))
+        [print(process) for process in processes]
